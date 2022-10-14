bab916ebcb1eea394d5dd69768f3e5121686126e
RusaRobot <102366548+RusaRobot@users.noreply.github.com>
2022-10-14 11:18:47 +0700

    Book collection tambah pagination (#4)
    * add book page and book collections
    
    * adding book collection and pagination

--- library-app/src/App.jsx
+++ library-app/src/App.jsx
@@ -14,8 +14,6 @@ import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import Book from "./pages/Book"
 
-
-
 const App = () => {
     return (
         // {Navbar}
@@ -120,11 +118,11 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/book" element={<Book />} />
             </Routes>
         </>
         //   {/* navbar */}
     )
-
 }
 
 export default App
