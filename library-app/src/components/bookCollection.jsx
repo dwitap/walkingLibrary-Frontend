8e4c988745715589f67beb6350b2f3b219c3e1ad
dwitap <111846245+dwitap@users.noreply.github.com>
2022-10-16 17:05:44 +0700

    FE book (#16)

--- library-app/src/components/bookCollection.jsx
+++ library-app/src/components/bookCollection.jsx
@@ -1,72 +1,63 @@
-import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
-  Avatar,
-  Box,
-  Button,
-  HStack,
-  Icon,
-  Image,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Stack,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react"
-
-import { useFormik } from "formik"
-import { BsThreeDots } from "react-icons/bs"
+import { Tr, Td, useDisclosure, Image, Button } from "@chakra-ui/react"
 import { useSelector } from "react-redux"
-import * as Yup from "yup"
 import { axiosInstance } from "../api"
 import { useState } from "react"
 import { useEffect } from "react"
+import DetailPage from "../pages/DetailBook"
 import { Link } from "react-router-dom"
 
-const BookCollection = ({ title, author, release_year, genre, language }) => {
-  const authSelector = useSelector((state) => state.auth)
+const BookCollection = ({
+    id,
+    image_url,
+    title,
+    author,
+    release_year,
+    genre,
+    language,
+}) => {
+    const authSelector = useSelector((state) => state.auth)
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
-  // const confirmDeleteBtnHandler = () => {
-  //   onClose()
-  //   onDelete()
-  // }
+    // const confirmDeleteBtnHandler = () => {
+    //   onClose()
+    //   onDelete()
+    // }
 
-  const [book, setBook] = useState([])
+    const [book, setBook] = useState([])
 
-  const fetchBooks = async () => {
-    try {
-      const collection = await axiosInstance.get("/book")
-      setBook(collection.data)
-    } catch (err) {
-      console.log(err)
+    const fetchBooks = async () => {
+        try {
+            const collection = await axiosInstance.get("/book")
+            setBook(collection.data.data)
+        } catch (err) {
+            console.log(err)
+        }
     }
-  }
 
-  useEffect(() => {
-    fetchBooks()
-  }, [])
+    useEffect(() => {
+        fetchBooks()
+    }, [])
 
-  return (
-    <>
-      
-      <HStack>
-        <Box>{title}</Box>
-        <Box>{author}</Box>
-        <Box>{release_year}</Box>
-        <Box>{genre}</Box>
-        <Box>{language}</Box>
-      </HStack>
-    </>
-  )
+    return (
+        <>
+            <Tr>
+                <Td>
+                    <Image maxH={"120px"} src={image_url || ""} />
+                </Td>
+                <Td>
+                    <Link to={`/detail`}>{title}</Link>
+                </Td>
+                <Td>{author}</Td>
+                <Td>{release_year}</Td>
+                <Td>{genre}</Td>
+                <Td>{language}</Td>
+                <Td>
+                    <Button>Add</Button>
+                </Td>
+            </Tr>
+        </>
+    )
 }
 
 export default BookCollection
