38d512828314d513209d26b909dfc35938734b9e
RusaRobot <agassuper@gmail.com>
2022-10-17 13:04:54 +0700

    adding book to cart

--- library-app/src/components/bookCollection.jsx
+++ library-app/src/components/bookCollection.jsx
@@ -1,4 +1,11 @@
-import { Tr, Td, useDisclosure, Image, Button } from "@chakra-ui/react"
+import {
+  Tr,
+  Td,
+  useDisclosure,
+  Image,
+  Button,
+  useToast,
+} from "@chakra-ui/react"
 import { useSelector } from "react-redux"
 import { axiosInstance } from "../api"
 import { useState } from "react"
@@ -7,59 +14,80 @@ import DetailPage from "../pages/DetailBook"
 import { Link } from "react-router-dom"
 
 const BookCollection = ({
-    id,
-    image_url,
-    title,
-    author,
-    release_year,
-    genre,
-    language,
+  id,
+  image_url,
+  title,
+  author,
+  release_year,
+  genre,
+  language,
 }) => {
-    const authSelector = useSelector((state) => state.auth)
+  const authSelector = useSelector((state) => state.auth)
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-    // const confirmDeleteBtnHandler = () => {
-    //   onClose()
-    //   onDelete()
-    // }
+  // const confirmDeleteBtnHandler = () => {
+  //   onClose()
+  //   onDelete()
+  // }
 
-    const [book, setBook] = useState([])
+  const toast = useToast()
 
-    const fetchBooks = async () => {
-        try {
-            const collection = await axiosInstance.get("/book")
-            setBook(collection.data.data)
-        } catch (err) {
-            console.log(err)
-        }
+  const [book, setBook] = useState([])
+//   const [addBook, setAddBook] = useState()
+
+  const fetchBooks = async () => {
+    try {
+      const collection = await axiosInstance.get("/book")
+      setBook(collection.data.data)
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    useEffect(() => {
-        fetchBooks()
-    }, [])
+  const pushToCart = async () => {
+    try {
+      let bookToAdd = {
+        BookId: id,
+      }
+      await axiosInstance.post("/cart", bookToAdd)
 
-    return (
-        <>
-            <Tr>
-                <Td>
-                    <Link to={`/detail/${id}`}>
-                        <Image maxH={"120px"} src={image_url || ""} />
-                    </Link>
-                </Td>
-                <Td>
-                    <Link to={`/detail/${id}`}>{title}</Link>
-                </Td>
-                <Td>{author}</Td>
-                <Td>{release_year}</Td>
-                <Td>{genre}</Td>
-                <Td>{language}</Td>
-                <Td>
-                    <Button>Add</Button>
-                </Td>
-            </Tr>
-        </>
-    )
+      toast({ title: "Book Added", status: "success" })
+    } catch (err) {
+      console.log(err)
+    }
+  } 
+
+  const addToCartBtn = () => {
+    pushToCart()
+
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  return (
+    <>
+      <Tr>
+        <Td>
+          <Link to={`/detail/${id}`}>
+            <Image maxH={"120px"} src={image_url || ""} />
+          </Link>
+        </Td>
+        <Td>
+          <Link to={`/detail/${id}`}>{title}</Link>
+        </Td>
+        <Td>{author}</Td>
+        <Td>{release_year}</Td>
+        <Td>{genre}</Td>
+        <Td>{language}</Td>
+        <Td>
+          <Button onClick = {addToCartBtn}>Add</Button>
+        </Td>
+      </Tr>
+    </>
+  )
 }
 
 export default BookCollection
